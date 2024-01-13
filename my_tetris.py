16f7e3f51d37877db8aff14505f8abd35f1f8908
jackberrypassionfruit <pashayanjack@gmail.com>
2024-01-13 14:15:46 -0500

    added rotation and row clearing functionality

--- my_tetris.py
+++ my_tetris.py
@@ -2,10 +2,13 @@ import sys, curses, time
 
 class MyTetris():
   def __init__(self):
+    self.board_width = 10
+    self.board_height = 20
+    
+    
     with open(sys.argv[1], 'r', encoding='utf8') as infile:
       jet_push_text = infile.read()
     self.hot_gas_movement = list(''.join(jet_push_text.split('\n')))
-    
     self.max_height = -1
     self.blocks = [
       ['@@@@'],
@@ -31,28 +34,23 @@ class MyTetris():
         '@@'
       ]
     ]
+    
     self.hot_gas_index = 0
-      
-    self.feeld = '''\
-.......
-.......
-.......
-.......\
-'''.split('\n')
-
+    self.feeld = ['.'*self.board_width for i in range(self.board_height)]
     self.block_carcasses = set()
     
   def __repr__(self):
+    print('\n\r'*30)
     result = ''
     for line_index, line in enumerate(self.feeld):
       block_row = line_index - self.current_loc[1]
-      # if self.current_loc == [-1,-1], then yet to drop a new block
-      if self.current_loc != [-1, -1] and 0 <= block_row < len(self.blocks[self.block_index]):
+      # if self.current_loc == [-1,-1], then about to drop a new block
+      if self.current_loc != [-1, -1] and 0 <= block_row < len(self.current_block):
         block_line = ''
         for col_index, col in enumerate(self.feeld[0]):
           block_col = col_index - self.current_loc[0]
-          if self.current_loc[0] <= col_index < self.current_loc[0] + len(self.blocks[self.block_index][block_row]):
-            block_row_col = self.blocks[self.block_index][block_row][block_col]
+          if self.current_loc[0] <= col_index < self.current_loc[0] + len(self.current_block[block_row]):
+            block_row_col = self.current_block[block_row][block_col]
             if block_row_col == '@':
               block_line += block_row_col
             else:
@@ -66,11 +64,11 @@ class MyTetris():
           
           
     # return result
-    return '\n\r'.join(reversed(result.split('\n\r'))) # (Un)Flip the image upside down
+    return '\n\r'.join(reversed(result.split('\n\r')))+'\n\r' # (Un)Flip the image upside down
   
   def get_current_block_coords(self):  
     block_coords = set()
-    for block_row_index, block_row in enumerate(self.blocks[self.block_index]):
+    for block_row_index, block_row in enumerate(self.current_block):
       for block_col_index, block_col in enumerate(block_row):
         if block_col != '.':
           new_block_spot_x, new_block_spot_y = self.current_loc
@@ -95,6 +93,24 @@ class MyTetris():
       potential_collisions.update([(collision_spot_x, collision_spot_y)])
     return potential_collisions.intersection(self.block_carcasses)
   
+  def remove_full_rows(self):
+    rows_to_remove = []
+    for row_index, row in enumerate(self.feeld):
+      if '.' not in row:          
+        rows_to_remove.append(row_index)
+      
+    for row_index in rows_to_remove:
+      self.feeld = self.feeld[:row_index] + self.feeld[row_index + 1:] + ['.'*self.board_width]
+      
+      carcasses_in_this_row = [coord for coord in self.block_carcasses if coord[1] == row_index]
+      for carcass in carcasses_in_this_row:
+        self.block_carcasses.remove(carcass)
+        
+      carcasses_above_this_row = [coord for coord in self.block_carcasses if coord[1] > row_index]
+      for carcass in carcasses_above_this_row:
+        self.block_carcasses.remove(carcass)
+        self.block_carcasses.update([(carcass[0], carcass[1]-1)])
+  
   def stop_block(self):
     # This method takes the current state of the block, and turns it into block carcasses
     block_coords = self.get_current_block_coords()
@@ -103,6 +119,9 @@ class MyTetris():
       self.feeld[coord_y] = self.feeld[coord_y][:coord_x] + '#' + self.feeld[coord_y][coord_x+1:]
     self.block_carcasses.update(block_coords)
     self.current_loc = [-1,-1]
+    # TODO
+    # create a function to find and remove all full rows
+    self.remove_full_rows()
     # Return a value, max_height, which will be max(current_max_height, max_height_of_this_dead_block)
     return max(self.max_height, max([block_coord[1] for block_coord in block_coords]))
   
@@ -112,7 +131,7 @@ class MyTetris():
         if self.current_loc[0] > 0:
           self.current_loc[0] -= 1
       case '>':
-        coord_right = self.current_loc[0] + len(self.blocks[self.block_index][0])
+        coord_right = self.current_loc[0] + len(self.current_block[0])
         if coord_right < len(self.feeld[0]):
           self.current_loc[0] += 1
         
@@ -124,7 +143,7 @@ class MyTetris():
     dir = None
     while self.current_loc != [-1, -1]:
       # move left or right, depending on wind instruction
-      if dir is not '^':
+      if dir != '^':
         if play_game:
           dir = None
           while not dir:
@@ -137,34 +156,33 @@ class MyTetris():
                 dir = '>'
               case 'w':
                 dir = '^'
+              case 'q':
+                self.current_block = self.rotate_block(self.current_block, clock_wise=False)
+                print(self)
+                print(f'self.current_loc: {self.current_loc}\r')
+              case 'e':
+                self.current_block = self.rotate_block(self.current_block, clock_wise=True)
+                print(self)
+                print(f'self.current_loc: {self.current_loc}\r')
               case _:
                 pass
         else:
           dir = self.hot_gas_movement[self.hot_gas_index]
           self.hot_gas_index = (self.hot_gas_index + 1) % len(self.hot_gas_movement)
       if not self.check_if_will_collide(dir) and dir in ['<', '>']:
-        self.hot_gas(dir)
-      # print('\n'*30)
-      # print(self)
-      # print('self.current_loc: ', self.current_loc)
-      # print('dir: ', dir)
-      # print('self.hot_gas_index: ', self.hot_gas_index)
-      # time.sleep(.25)
-      
+        self.hot_gas(dir)      
       if self.current_loc[1] == 0 or self.check_if_will_collide('V'):
         self.max_height = self.stop_block()
       else:
         self.current_loc[1] -= 1
-      print('\n\r'*30)
       print(self)
-      print('self.current_loc: ', self.current_loc)
+      print(f'self.current_loc: {self.current_loc}\r')
       if not play_game:
         time.sleep(.25)
       
   def release_block(self, play_game = False):
-    current_block_height = len(self.blocks[self.block_index])
-    self.feeld += ['.......'] * (current_block_height)
-    self.current_loc = [2, self.max_height + 4]
+    current_block_height = len(self.current_block)
+    self.current_loc = [self.board_width//2, self.board_height - current_block_height]
     self.block_fall_down(play_game=play_game)    
     
   def release_the_blocks(self):
@@ -179,5 +197,6 @@ class MyTetris():
     i = 0
     while True:
       self.block_index = i % len(self.blocks)
+      self.current_block = self.blocks[self.block_index]
       self.release_block(play_game = True)
       i += 1
--- test.py
+++ test.py
@@ -1,48 +1,27 @@
-# from my_tetris import MyTetris
+from my_tetris import MyTetris
+import curses
 
-# falling_rocks = MyTetris()
+falling_rocks = MyTetris()
 
 # falling_rocks.release_the_blocks()
-# falling_rocks.play_game()
+try:
+  falling_rocks.play_game()
+except Exception as e:
+  curses.endwin()
+  print(e)
+except:
+  curses.endwin()
 
+
+# """
+# TODO
+# 1. implement rotation controls
+#   - does not occur about center, which is awkward
+#   - can put blocks out of the right bounds, which is incorrect
+# 2. add a win/lose condition
+# """
 
 # block_90 = []
 # for col_index in range(len(block[0])):
 #   new_row = ''.join([row[col_index] for row in block[::-1]])
 #   block_90.append(new_row)
-
-def rotate_block(block, clock_wise=True):
-  clock_wise = 1 if clock_wise else -1
-  return [ ''.join([ row[col_index] for row in block[::-1*clock_wise] ]) for col_index in range(len(block[0]))[::clock_wise] ]
-
-block = [
-        '@@@',
-        '..@',
-        '..@'
-      ]
-
-for row in block:
-  print(row)
-print()
-
-block = rotate_block(block, clock_wise=True)
-
-for row in block:
-  print(row)
-print()
-  
-block = rotate_block(block, clock_wise=False)
-block = rotate_block(block, clock_wise=False)
-
-for row in block:
-  print(row)
-print()
-
-# """
-# TODO
-# 1. implement rotation controls
-# 2. solved curses problem in the console after running this script
-#   - the /r (carriage return) symbol is not applied correctly after running "curses.initscr().getkey()"
-# 3. add a win/lose condition
-#   - clear lines when full
-# """
